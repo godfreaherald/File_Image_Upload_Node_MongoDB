e7d9effea55007f086ea73e6e01d1b9729412882
godfreaherald <wambusiness2015@gmail.com>
2019-10-25 12:17:18 +0300

    file upload to and download from  mongoDB

--- server.js
+++ server.js
@@ -1,6 +1,11 @@
 const express = require('express');
 const multer = require('multer');
 const dotenv = require('dotenv');
+const path = require('path');
+const fs = require('fs');
+const dbConfig = require('./config');
+const MongoClient = require('mongodb').MongoClient;
+ObjectId = require('mongodb').ObjectId;
 const app = express();
 
 dotenv.config();
@@ -17,7 +22,14 @@ let storage = multer.diskStorage({
   }
 });
 
-const upload = multer(storage);
+const upload = multer({ storage });
+
+MongoClient.connect(dbConfig.URI, (err, client) => {
+  if (err) return console.log(err);
+  db = client.db('test');
+
+  console.log('mongoDB connected ');
+});
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
@@ -43,6 +55,54 @@ app.post('/uploadFiles', upload.array('myFiles', 12), (req, res, next) => {
   }
 
   res.send(files);
+});
+
+app.post('/uploadPhoto', upload.single('picture'), (req, res) => {
+  var img = fs.readFileSync(req.file.path);
+  var encode_image = img.toString('base64');
+  // Define a JSONobject for the image attributes for saving to database
+
+  var finalImg = {
+    contentType: req.file.mimetype,
+    image: new Buffer(encode_image, 'base64')
+  };
+  db.collection('quotes').insertOne(finalImg, (err, result) => {
+    console.log(result);
+
+    if (err) return console.log(err);
+
+    console.log('saved to database');
+    res.redirect('/');
+  });
+});
+
+app.get('/photos', (req, res) => {
+  db.collection('quotes')
+    .find()
+    .toArray((err, result) => {
+      const imgArray = result.map(element => element._id);
+      if (err) console.log(err);
+      res.send(imgArray);
+    });
+});
+
+app.get('/photo/:id', (req, res) => {
+  db.collection('quotes').findOne(
+    { _id: ObjectId(req.params.id) },
+    (err, result) => {
+      if (err) console.log(err);
+      res.contentType('image/jpeg');
+      res.send(result.image.buffer);
+    }
+  );
+});
+
+// define a route to download a file
+app.get('/download/:file(*)', (req, res) => {
+  var file = req.params.file;
+  var fileLocation = path.join('./uploads', file);
+  console.log(fileLocation);
+  res.download(fileLocation, file);
 });
 
 app.listen(port, () => {
